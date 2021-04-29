9a714e3eafc6f82ca83c7ddce6db06e3bcadbe3c
OJL <1791088334@qq.com>
2021-04-29 13:54:27 +0800

    logo修改

--- app/src/main/java/com/example/mdtest/login/LogActivity.java
+++ app/src/main/java/com/example/mdtest/login/LogActivity.java
@@ -13,9 +13,9 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.mdtest.Bean.MyUser;
 import com.example.mdtest.MainActivity;
 import com.example.mdtest.R;
-import com.example.mdtest.Bean.MyUser;
 
 import cn.bmob.v3.Bmob;
 import cn.bmob.v3.BmobUser;
@@ -24,7 +24,11 @@ import cn.bmob.v3.listener.SaveListener;
 
 public class LogActivity extends AppCompatActivity {
 
+//    private SharedPreferences pref;
+//    private SharedPreferences.Editor editor;
+
     private Button btn_log;     //登录按钮
+    //private CheckBox rememberPass;      //记住密码复选框
 
     private TextView tv_reg;        //注册选项
     private TextView tv_psw_find;       //密码找回选项
@@ -38,6 +42,8 @@ public class LogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity_log);
+        //pref = PreferenceManager.getDefaultSharedPreferences(this);
+
         Bmob.resetDomain("https://open3.bmob.cn/8/");
         Bmob.initialize(this, "36d91b12a94a333fbd7281eb1265abb3");      //数据库初始化
 
@@ -55,6 +61,8 @@ public class LogActivity extends AppCompatActivity {
         btn_log = findViewById(R.id.bt_log);
         tv_reg = findViewById(R.id.tv_reg);
         tv_psw_find = findViewById(R.id.tv_find_psw);
+        //rememberPass = findViewById(R.id.remember_pass);
+        //boolean isRemember = pref.getBoolean("记住密码", false);
 
         // "注册界面"跳转
         tv_reg.setOnClickListener(new View.OnClickListener() {
@@ -83,6 +91,11 @@ public class LogActivity extends AppCompatActivity {
             startActivity(intent4);
             LogActivity.this.finish();      //销毁登录界面
         } else {
+//            if (isRemember) {
+//                MyUser myUser = MyUser.getCurrentUser(MyUser.class);
+//                String account = pref.getString("account", myUser.getMobilePhoneNumber());
+//                String password = pref.getString("password", myUser.getPassword);
+//            }
             // 用户未登录或未注册，跳转至"登录界面"
             btn_log.setOnClickListener(new View.OnClickListener() {
                 @Override
